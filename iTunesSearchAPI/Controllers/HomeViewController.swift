8648befcd2ef7ed5c5b85a57997ae28c60469f26
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-11-05 12:52:06 -0400

    Prepping for home view for API integration

---
+++ iTunesSearchAPI/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  iTunesSearchAPI
+//
+//  Created by Yaz Burrell on 11/5/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iTunesSearchAPI/ViewModel/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  iTunesSearchAPI
+//
+//  Created by Yaz Burrell on 11/5/22.
+//
+
+import Foundation
+
+extension HomeViewController {
+    
+    class HomeViewModel {
+        // MARK: - Properties
+        
+        // MARK: - API Actions
+        
+        // MARK: - API Data source
+        
+    }
+    
+}
