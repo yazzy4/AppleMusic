79d1669ae6c6bd1df8bb9528a54bf3449e930908
Yaz Burrell <yazburrell@Yazs-MacBook-Pro.local>
2022-11-05 13:04:58 -0400

    Working on implementing API call inview model

--- iTunesSearchAPI/ViewModel/HomeViewModel.swift
+++ iTunesSearchAPI/ViewModel/HomeViewModel.swift
@@ -11,11 +11,47 @@ extension HomeViewController {
     
     class HomeViewModel {
         // MARK: - Properties
+        private var results: [Release] = []
+        
+        var albumCoverSize: CGSize = CGSize(width: 100, height: 100)
+        
+        var onReseultsReceived: (() -> Void)?
+        
+        var onError: ((String) -> Void)?
         
         // MARK: - API Actions
         
+        func fetchResults(with query: String) {
+            // Add API call here
+            print("DEBUG: search not implemented")
+        }
+        
+        func handleReleaseSection(at indexPath: IndexPath) {
+            guard indexPath.row >= 0 && indexPath.row < results.count else { return }
+            print("DEBUG: section not implemented")
+        }
+        
         // MARK: - API Data source
         
+        func coverUrl(for indexPath: IndexPath) -> URL? {
+            guard indexPath.row >= 0 && indexPath.row < results.count else { return nil }
+            
+            return results[indexPath.row].imageUrl
+        }
+        
+        func numberOfResults() -> Int {
+            results.count
+        }
+       
     }
     
+}
+
+extension HomeViewController.HomeViewModel {
+    struct Release {
+        var id: Int
+        var imageUrl: URL
+        var title: String
+        var artistID: Int
+    }
 }
